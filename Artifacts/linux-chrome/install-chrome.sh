125cbdb02e7e0a6655c14234b6a3c6980f5368c8
Alex Perovich <alperovi@microsoft.com>
2016-11-15 13:58:58 -0800

    Add -y to install commands for chrome

--- Artifacts/linux-chrome/install-chrome.sh
+++ Artifacts/linux-chrome/install-chrome.sh
@@ -6,17 +6,16 @@ if [ \( $ARCH != "x86_64" \) -a \( $ARCH != "amd64" \) ]; then
   exit -1
 fi
 
-add-apt-repository universe
-apt-get update
-sudo apt-get install libgconf2-4 libnss3-1d libxss1
-
 if [ $(which apt) ]; then
+  add-apt-repository universe
+  apt-get update
+  sudo apt-get install libgconf2-4 libnss3-1d libxss1 -y -qq
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -i google-chrome-stable_current_amd64.deb
-  apt-get install -f
+  apt-get install -f -y -qq
 elif [ $(which yum) ]; then
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-  yum install google-chrome-stable_current_x86_64.rpm
+  yum -y install google-chrome-stable_current_x86_64.rpm
 else
   echo "Could not discover package manager" 1>&2
   exit -1
