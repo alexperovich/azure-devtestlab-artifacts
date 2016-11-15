3615b93237cd264d3cf960b16807091f5b72c727
Alex Perovich <alperovi@microsoft.com>
2016-11-14 17:34:54 -0800

    Be verbose

--- Artifacts/linux-rdp/install-xrdp-with-log.sh
+++ Artifacts/linux-rdp/install-xrdp-with-log.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/bin/sh install-xrdp.sh "$@" 2>&1 | install -D /dev/stdin /var/log/azure-dtl-artifacts/linux-rdp.log
+/bin/sh -v install-xrdp.sh "$@" 2>&1 | install -D /dev/stdin /var/log/azure-dtl-artifacts/linux-rdp.log
